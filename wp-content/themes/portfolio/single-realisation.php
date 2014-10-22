f55e0f293eb13bccb651ef904f449cdcaec8fc3c
maximeguioneau <mguioneau@gmail.com>
2014-10-22 13:34:58 +0200

    Avancement single realisation

--- wp-content/themes/portfolio/single-realisation.php
+++ wp-content/themes/portfolio/single-realisation.php
@@ -1,7 +1,5 @@
 <?php get_header(); ?>
 
-<?php var_dump($post); ?>
-
 
 <div class="single-project">
 
@@ -15,7 +13,7 @@
 				<?php $promotion = get_term( get_field('promotion', $post->ID), 'promotion'); ?>
 
 				<?php if (!empty($promotion->name)): ?>
-					<span><?= $promotion->name; ?></span>
+					<span>P<?= $promotion->name; ?> -</span>
 				<?php endif ?>
 
 				<?php $annee = get_term( get_field('annee', $post->ID), 'annee'); ?>
@@ -33,10 +31,9 @@
 				<?php
 					$imgid = get_post_meta($post->ID, 'image_principal', true);
 					$img = wp_get_attachment_image_src($imgid, 'single-size');
-					var_dump($img);
 				?>
-				<?php if(!empty($image)): //dont output an empty image tag ?>
-
+				<?php if(!empty($img)): //dont output an empty image tag ?>
+					<img src="<?= $img[0]; ?>" width="<?= $img[1]; ?>" height="<?= $img[2]; ?>">
 				<?php endif; ?>
 			</div>
 
@@ -45,7 +42,9 @@
 		<div class="block-right">
 
 			<div class="date">
-				<span><?php the_field('date_de_realisation'); ?></span>
+				<?php if (!empty(the_field('date_de_realisation'))): ?>
+					<span><?php the_field('date_de_realisation'); ?></span>
+				<?php endif; ?>
 			</div>
 
 			<div class="url">
@@ -53,37 +52,45 @@
 			</div>
 
 			<div class="client">
-				<span><?php the_field('client'); ?></span>
+				<?php if (!empty(the_field('client'))): ?>
+					<span><?php the_field('client'); ?></span>
+				<?php endif; ?>
 			</div>
 
 			<div class="description">
-				<span><p><?php the_content(); ?></p></span>
+				<?php if (!empty(the_content())): ?>
+					<span><p><?php the_content(); ?></p></span>
+				<?php endif; ?>
 			</div>
 
 			<div class="createur">
-				<span>Membres : <?php
-				/*
-				$terms = get_term( get_field('membres_dequipe', $post->ID), 'membres_dequipe');
-				foreach ( $terms as $term ) {
-				    echo $term->name." ";
-				}*/
-				?></span>
+				<span>
+				<?php if( have_rows('membres_dequipe') ): ?>
+			 
+				    <?php while( have_rows('membres_dequipe') ): the_row(); ?>
+				 
+						<span><?php the_sub_field('prenom'); ?> <?php the_sub_field('nom'); ?>, </span>
+				        
+				    <?php endwhile; ?>
+			 
+				<?php endif; ?>
+				</span>
 			</div>
 
 			<div class="technologie">
-				<span>Technologies : <?php
+				<span>
+				
+				<?php if( get_field('technologies') ): ?>
+				    <p><?php the_field('technologies'); ?></p>
+				<?php endif; ?>
 
-				$terms = get_term( get_field('technologies', $post->ID), 'technologies');
-				foreach ( $terms as $term ) {
-				    echo $term->name." ";
-				}
-
-
-				 ?></span>
+				</span>
 			</div>
 
 			<div class="categorie">
-				<span>Catégories : <?php the_field('category'); ?></span>
+				<?php if( get_field('category') ): ?>
+					<span><?php the_field('category'); ?></span>
+				<?php endif; ?>
 			</div>
 
 		</div>
@@ -97,6 +104,28 @@
 	<h1>Autres Réalisations</h1>
 
 	<div class="gallery">
+
+		<?php $realisations = new WP_Query(array(
+			'post_type' => 'realisation',
+			'orderby' => array('rand'),
+			'posts_per_page' => 6
+		)); ?>
+
+		<?php if ( $realisations->have_posts() ) : while ( $realisations->have_posts() ) : $realisations->the_post(); ?>
+		<a href="<?php the_permalink() ?>">
+			<div class="block-realisation">
+				<?php if (!empty(get_field('image_principal', $post->ID))): ?>
+					<img src="<?php the_field('image_principal'); ?>" alt="">
+				<?php endif ?>
+				<div class="info-realisation">
+					<span class="name"><?php the_title(); ?></span>
+				</div>
+
+			</div>
+		</a>
+	<?php endwhile; else: ?>
+		<p>Il n'existe aucun projet pour le moment</p>
+	<?php endif; ?>
 
 	</div>
 
